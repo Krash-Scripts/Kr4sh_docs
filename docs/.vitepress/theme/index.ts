9d2bc4d2eb027bc4c208059aae1d529291ab09a9
Krash <ksnexi@gmail.com>
2026-08-11 01:17:43 +0200

    Integrate minimalist KR4SH docs theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
 import './custom.css'
+import BrandStrip from './components/BrandStrip.vue'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('BrandStrip', BrandStrip)
+  }
+} satisfies Theme
