b01887dfae813caf19c17a90d08659907bd68b12
Krash <ksnexi@gmail.com>
2026-08-11 01:18:21 +0200

    Add K_Scripts brand strip

---
+++ docs/.vitepress/theme/components/BrandStrip.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="kr-brand-strip">
+    <div class="kr-brand-strip__main">
+      <img :src="withBase('/k_scripts-logo.webp')" alt="K_Scripts" class="kr-brand-strip__logo" />
+      <div class="kr-brand-strip__copy">
+        <div class="kr-brand-strip__eyebrow">Powered by K_Scripts</div>
+        <div class="kr-brand-strip__title">Oficjalny hub dokumentacji KR4SH</div>
+      </div>
+    </div>
+    <div class="kr-brand-strip__meta">
+      <span>FiveM</span>
+      <span>ESX</span>
+      <span>Docs v1</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { withBase } from 'vitepress'
+</script>
